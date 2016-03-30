066c8a5bf3b91a3932cd6721b685bc82a07e398d
Ricardo Coelho <ricardocoelho22@gmail.com>
2016-03-30 19:47:28 +0100

    Updated DeclaredServerResources, add test client
    Updated DeclaredServerResources to improve path declaration
    Created client folder with test page to test resources

--- bankServer/src/main/java/cpslabteam/bank/webservice/DeclaredServerResources.java
+++ bankServer/src/main/java/cpslabteam/bank/webservice/DeclaredServerResources.java
@@ -23,24 +23,37 @@ public final class DeclaredServerResources {
 
 	private static Map<String, Class<? extends ServerResource>> serverResources;
 
+	private static final String CUSTOMERS_BASE_PATH = "/customers";
+	private static final String CUSTOMER_BASE_PATH = "/{customer}";
+	private static final String DEPOSITORS_BASE_PATH = "/depositors";
+	private static final String DEPOSITOR_BASE_PATH = "/{depositor}";
+	private static final String BORROWERS_BASE_PATH = "/borrowers";
+	private static final String BORROWER_BASE_PATH = "/{borrower}";
+	private static final String BRANCHES_BASE_PATH = "/branches";
+	private static final String BRANCH_BASE_PATH = "/{branch}";
+	private static final String ACCOUNTS_BASE_PATH = "/accounts";
+	private static final String ACCOUNT_BASE_PATH = "/{account}";
+	private static final String LOANS_BASE_PATH = "/loans";
+	private static final String LOAN_BASE_PATH = "/{loan}";
+
 	static {
 		serverResources = new LinkedHashMap<>();
 		declareServerResources();
 	}
 
 	private static void declareServerResources() {
-		serverResources.put("/customers", CustomersResource.class);
-		serverResources.put("/customers/{customer}", CustomerResource.class);
-		serverResources.put("/depositors", DepositorsResource.class);
-		serverResources.put("/depositors/{depositor}", DepositorResource.class);
-		serverResources.put("/borrowers", BorrowersResource.class);
-		serverResources.put("/borrowers/{borrower}", BorrowerResource.class);
-		serverResources.put("/branches", BranchesResource.class);
-		serverResources.put("/branches/{branch}", BranchResource.class);
-		serverResources.put("/accounts", AccountsResource.class);
-		serverResources.put("/accounts/{account}", AccountResource.class);
-		serverResources.put("/loans", LoansResource.class);
-		serverResources.put("/loans/{loan}", LoanResource.class);
+		serverResources.put(CUSTOMERS_BASE_PATH, CustomersResource.class);
+		serverResources.put(CUSTOMERS_BASE_PATH + CUSTOMER_BASE_PATH, CustomerResource.class);
+		serverResources.put(DEPOSITORS_BASE_PATH, DepositorsResource.class);
+		serverResources.put(DEPOSITORS_BASE_PATH + DEPOSITOR_BASE_PATH, DepositorResource.class);
+		serverResources.put(BORROWERS_BASE_PATH, BorrowersResource.class);
+		serverResources.put(BORROWERS_BASE_PATH + BORROWER_BASE_PATH, BorrowerResource.class);
+		serverResources.put(BRANCHES_BASE_PATH, BranchesResource.class);
+		serverResources.put(BRANCHES_BASE_PATH + BRANCH_BASE_PATH, BranchResource.class);
+		serverResources.put(ACCOUNTS_BASE_PATH, AccountsResource.class);
+		serverResources.put(ACCOUNTS_BASE_PATH + ACCOUNT_BASE_PATH, AccountResource.class);
+		serverResources.put(LOANS_BASE_PATH, LoansResource.class);
+		serverResources.put(LOANS_BASE_PATH + LOAN_BASE_PATH, LoanResource.class);
 	}
 
 	public static Map<String, Class<? extends ServerResource>> getDeclaredServerResources() {
