0155fce95812e2e04c8e075f23f50ee521c11d88
Ricardo Coelho <ricardocoelho22@gmail.com>
2016-05-01 16:09:03 +0100

    [UPD] updated according to utils bank-integration

--- bank/src/main/java/cpslab/bank/internal/dao/HibernateAccountDAO.java
+++ bank/src/main/java/cpslab/bank/internal/dao/HibernateAccountDAO.java
@@ -9,69 +9,67 @@ import cpslab.bank.api.dao.AccountDAO;
 import cpslab.bank.api.entities.Account;
 import cpslab.util.db.hibernate.HibernateDao;
 
-public class HibernateAccountDAO extends
-        HibernateDao<Account>
-        implements AccountDAO {
+public class HibernateAccountDAO extends HibernateDao<Account>implements AccountDAO {
 
-    private final static String CUSTOMER_ACCOUNTS_QUERY =
-        "SELECT ac FROM Customer c JOIN c.accounts ac WHERE c.id = :id";
+	private final static String CUSTOMER_ACCOUNTS_QUERY =
+			"SELECT ac FROM Customer c JOIN c.accounts ac WHERE c.id = :id";
 
-    private final static String BRANCH_ACCOUNTS_QUERY =
-        "SELECT ac FROM Branch b JOIN b.accounts ac WHERE b.id = :id";
+	private final static String BRANCH_ACCOUNTS_QUERY =
+			"SELECT ac FROM Branch b JOIN b.accounts ac WHERE b.id = :id";
 
-    private final static String CUSTOMER_ACCOUNT_QUERY =
-        "SELECT ac FROM Customer c JOIN c.accounts ac WHERE c.id = :customer_id AND ac.id = :account_id";
+	private final static String CUSTOMER_ACCOUNT_QUERY =
+			"SELECT ac FROM Customer c JOIN c.accounts ac WHERE c.id = :customer_id AND ac.id = :account_id";
 
-    private final static String BRANCH_ACCOUNT_QUERY =
-        "SELECT ac FROM Branch b JOIN b.accounts ac WHERE b.id = :branch_id AND ac.id = :account_id";
+	private final static String BRANCH_ACCOUNT_QUERY =
+			"SELECT ac FROM Branch b JOIN b.accounts ac WHERE b.id = :branch_id AND ac.id = :account_id";
 
-    /**
-     * Make sure it can be instantiated thorugh reflection.
-     */
-    public HibernateAccountDAO() {
-    }
+	/**
+	 * Make sure it can be instantiated thorugh reflection.
+	 */
+	public HibernateAccountDAO() {
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<Account> findCustomerAccounts(Long customerID) {
-        
-        Query query = getSession().createQuery(CUSTOMER_ACCOUNTS_QUERY);
-        query.setLong("id", customerID);
-        return query.list();
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Account> findCustomerAccounts(Long customerID) {
 
-    @Override
-    public Account findCustomerAccount(Long customerID, Long accountID) {
-        Query query = getSession().createQuery(CUSTOMER_ACCOUNT_QUERY);
-        query.setLong("customer_id", customerID);
-        query.setLong("account_id", accountID);
-        Account account = (Account) query.uniqueResult();
-        if (account != null) {
-            return account;
-        } else {
-            throw new ObjectNotFoundException(accountID, getPersistentClass().getName());
-        }
-    }
+		Query query = getSession().createQuery(CUSTOMER_ACCOUNTS_QUERY);
+		query.setLong("id", customerID);
+		return query.list();
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<Account> findBranchAccounts(Long branchID) {
-        Query query = getSession().createQuery(BRANCH_ACCOUNTS_QUERY);
-        query.setLong("id", branchID);
-        return query.list();
-    }
+	@Override
+	public Account findCustomerAccount(Long customerID, Long accountID) {
+		Query query = getSession().createQuery(CUSTOMER_ACCOUNT_QUERY);
+		query.setLong("customer_id", customerID);
+		query.setLong("account_id", accountID);
+		Account account = (Account) query.uniqueResult();
+		if (account != null) {
+			return account;
+		} else {
+			throw new ObjectNotFoundException(accountID, getPersistentClass().getName());
+		}
+	}
 
-    @Override
-    public Account findBranchAccount(Long branchID, Long accountID) {
-        Query query = getSession().createQuery(BRANCH_ACCOUNT_QUERY);
-        query.setLong("branch_id", branchID);
-        query.setLong("account_id", accountID);
-        Account account = (Account) query.uniqueResult();
-        if (account != null) {
-            return account;
-        } else {
-            throw new ObjectNotFoundException(accountID, getPersistentClass().getName());
-        }
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Account> findBranchAccounts(Long branchID) {
+		Query query = getSession().createQuery(BRANCH_ACCOUNTS_QUERY);
+		query.setLong("id", branchID);
+		return query.list();
+	}
+
+	@Override
+	public Account findBranchAccount(Long branchID, Long accountID) {
+		Query query = getSession().createQuery(BRANCH_ACCOUNT_QUERY);
+		query.setLong("branch_id", branchID);
+		query.setLong("account_id", accountID);
+		Account account = (Account) query.uniqueResult();
+		if (account != null) {
+			return account;
+		} else {
+			throw new ObjectNotFoundException(accountID, getPersistentClass().getName());
+		}
+	}
 
 }
--- bank/src/main/java/cpslab/bank/rest/api/BankRestServerActivator.java
+++ bank/src/main/java/cpslab/bank/rest/api/BankRestServerActivator.java
@@ -1,10 +1,8 @@
 package cpslab.bank.rest.api;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.restlet.Application;
 import org.restlet.Component;
@@ -28,148 +26,159 @@ import cpslab.bank.internal.dao.HibernateBranchDAO;
 import cpslab.bank.internal.dao.HibernateCustomerDAO;
 import cpslab.bank.internal.dao.HibernateLoanDAO;
 import cpslab.util.db.Repository;
+import cpslab.util.db.Repository.Dialect;
 import cpslab.util.db.RepositoryService;
+import cpslab.util.db.RepositoryService.Orm;
 import cpslab.util.db.hibernate.jackson.HibernateJacksonConverter;
 
-public final class BankRestServerActivator extends
-        Application {
+public final class BankRestServerActivator extends Application {
 
-    private static Logger logger = LoggerFactory.getLogger(BankRestServerActivator.class);
+	private static Logger logger = LoggerFactory.getLogger(BankRestServerActivator.class);
 
-    private static Component component;
+	private static Component component;
 
-    private static final BankRestServerActivator singleton = new BankRestServerActivator();
+	private static final BankRestServerActivator singleton = new BankRestServerActivator();
 
-    private static final int SERVER_PORT = 9192;
+	private static final int SERVER_PORT = 9192;
 
-    private static final String ROOT_ADDRESS = "http://localhost:" + SERVER_PORT;
-    
-    private static final String DATABASE_URL = "localhost:5432/bank";
-    
-    private static final String DATABASE_USERNAME = "postgres";
-    
-    private static final String DATABASE_PASSWORD = "root";
+	private static final String ROOT_ADDRESS = "http://localhost:" + SERVER_PORT;
 
-    public static void close() throws Exception {
-        new Thread() {
-            public void run() {
-                logger.info("Bundle closing, stopping REST server.");
-                try {
-                    component.stop();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-    }
+	private final String DATABASE_URL = "localhost:5432/bank";
 
-    private void registerDaos() {
-    	RepositoryService.initializeRepository(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
-        Repository r = RepositoryService.getInstance();
-        r.registerDao(Account.class, HibernateAccountDAO.class);
-        r.registerDao(Loan.class, HibernateLoanDAO.class);
-        r.registerDao(Branch.class, HibernateBranchDAO.class);
-        r.registerDao(Customer.class, HibernateCustomerDAO.class);
-    }
+	private final String DATABASE_USERNAME = "postgres";
 
-    public static BankRestServerActivator getInstance() {
-        return singleton;
-    }
+	private final String DATABASE_PASSWORD = "root";
 
-    public static void open() throws Exception {
-        new Thread() {
-            public void run() {
-                try {
-                    serverStart();
-                    serverAttach();
-                    printAvailableEndpoints();
-                } catch (InterruptedException e) {
-                    logger.info("Bundle closing, stopping REST server.");
-                }
-            }
-        }.start();
-    }
+	private final Dialect DATABASE_DIALECT = Dialect.POSTGRESQL;
 
-    private static void printAvailableEndpoints() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("You can use this REST API by addressing the following endpoints:\n");
-        List<String> endpoints = new ArrayList<>();
-        Set<String> serverResourcesURLs = DeclaredServerResources.getDeclaredServerResourcesURLs();
-        for (String url : serverResourcesURLs) {
-            stringBuilder.append("	" + ROOT_ADDRESS + "{}\n");
-            endpoints.add(url);
-        }
-        logger.info(stringBuilder.toString(), endpoints.toArray());
-    }
+	private final Orm DATABASE_ORM = Orm.HIBERNATE;
 
-    private static void serverAttach() {
-        component.getDefaultHost().attach(getInstance());
-    }
+	public static void close() throws Exception {
+		new Thread() {
+			public void run() {
+				logger.info("Bundle closing, stopping REST server.");
+				try {
+					component.stop();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+	}
 
-    private static void serverStart() throws InterruptedException {
-        component = new Component();
+	private void initializeRepository() {
+		RepositoryService.initializeRepository(DATABASE_ORM, DATABASE_DIALECT, DATABASE_URL,
+				DATABASE_USERNAME, DATABASE_PASSWORD);
+	}
 
-        component.getServers().add(Protocol.HTTP, SERVER_PORT);
+	private void registerDaos() {
+		Repository r = RepositoryService.getInstance();
+		r.registerDao(Account.class, HibernateAccountDAO.class);
+		r.registerDao(Loan.class, HibernateLoanDAO.class);
+		r.registerDao(Branch.class, HibernateBranchDAO.class);
+		r.registerDao(Customer.class, HibernateCustomerDAO.class);
+	}
 
-        boolean serverBound = false;
-        while (!serverBound) {
-            try {
-                logger.info("Bundle started, REST server initializing...");
-                component.start();
-                if (component.isStarted())
-                    logger.info("REST Server Started in port {}.", SERVER_PORT);
-                serverBound = true;
-            } catch (Exception e) {
-                try {
-                    component.stop();
-                } catch (Exception e1) {
-                    e1.printStackTrace();
-                }
-                logger.error(
-                        "Port {} is occupied! Check if a server is running already. Trying again in 15 sec.",
-                        SERVER_PORT);
-                Thread.sleep(15000);
-            }
-        }
-    }
+	public static BankRestServerActivator getInstance() {
+		return singleton;
+	}
 
-    private BankRestServerActivator() {
-        registerDaos();
-        configureCorsService();
-        setStatusService(new BankStatusService());
-        setHibernateJacksonConverter();
-    }
+	public static void open() throws Exception {
+		new Thread() {
+			public void run() {
+				try {
+					serverStart();
+					serverAttach();
+					printAvailableEndpoints();
+				} catch (InterruptedException e) {
+					logger.info("Bundle closing, stopping REST server.");
+				}
+			}
+		}.start();
+	}
 
-    private void setHibernateJacksonConverter() {
-        List<ConverterHelper> converters = Engine.getInstance().getRegisteredConverters();
-        JacksonConverter jacksonConverter = new JacksonConverter();
-        for (ConverterHelper converter : converters) {
-            if (converter instanceof JacksonConverter) {
-                jacksonConverter = (JacksonConverter) converter;
-                break;
-            }
-        }
-        if (jacksonConverter != null) {
-            converters.remove(jacksonConverter);
-            converters.add(new HibernateJacksonConverter());
-        }
-    }
+	private static void printAvailableEndpoints() {
+		String intro = "You can use this REST API by addressing the following endpoints:\n";
+		String endpoint = ("	" + ROOT_ADDRESS + "%s\n");
+		String endpoints = DeclaredServerResources	.getDeclaredServerResourcesURLs()
+													.stream()
+													.map(url -> String.format(endpoint, url))
+													.reduce("", String::concat);
+		logger.info(intro + endpoints);
+	}
 
-    private void configureCorsService() {
-        CorsService corsService = new CorsService();
-        corsService.setAllowedCredentials(false);
-        corsService.setDefaultAllowedMethods(new HashSet<Method>(
-                Arrays.asList(Method.GET, Method.PUT, Method.POST, Method.DELETE, Method.OPTIONS)));
-        corsService.setAllowedOrigins(new HashSet<>(Arrays.asList("*")));
-        corsService.setAllowedHeaders(new HashSet<>(Arrays.asList("Content-Type")));
-        getServices().add(corsService);
-    }
+	private static void serverAttach() {
+		component	.getDefaultHost()
+					.attach(getInstance());
+	}
 
-    @Override
-    public synchronized Restlet createInboundRoot() {
-        final Router router = new Router(getContext());
-        DeclaredServerResources.getDeclaredServerResources()
-                .forEach((url, resource) -> router.attach(url, resource));
-        return router;
-    }
+	private static void serverStart() throws InterruptedException {
+		component = new Component();
+
+		component	.getServers()
+					.add(Protocol.HTTP, SERVER_PORT);
+
+		boolean serverBound = false;
+		while (!serverBound) {
+			try {
+				logger.info("Bundle started, REST server initializing...");
+				component.start();
+				if (component.isStarted())
+					logger.info("REST Server Started in port {}.", SERVER_PORT);
+				serverBound = true;
+			} catch (Exception e) {
+				try {
+					component.stop();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				logger.error(
+						"Port {} is occupied! Check if a server is running already. Trying again in 15 sec.",
+						SERVER_PORT);
+				Thread.sleep(15000);
+			}
+		}
+	}
+
+	private BankRestServerActivator() {
+		initializeRepository();
+		registerDaos();
+		configureCorsService();
+		setStatusService(new BankStatusService());
+		setHibernateJacksonConverter();
+	}
+
+	private void setHibernateJacksonConverter() {
+		List<ConverterHelper> converters = Engine	.getInstance()
+													.getRegisteredConverters();
+		JacksonConverter jacksonConverter = new JacksonConverter();
+		for (ConverterHelper converter : converters) {
+			if (converter instanceof JacksonConverter) {
+				jacksonConverter = (JacksonConverter) converter;
+				break;
+			}
+		}
+		if (jacksonConverter != null) {
+			converters.remove(jacksonConverter);
+			converters.add(new HibernateJacksonConverter());
+		}
+	}
+
+	private void configureCorsService() {
+		CorsService corsService = new CorsService();
+		corsService.setAllowedCredentials(false);
+		corsService.setDefaultAllowedMethods(new HashSet<Method>(
+				Arrays.asList(Method.GET, Method.PUT, Method.POST, Method.DELETE, Method.OPTIONS)));
+		corsService.setAllowedOrigins(new HashSet<>(Arrays.asList("*")));
+		corsService.setAllowedHeaders(new HashSet<>(Arrays.asList("Content-Type")));
+		getServices().add(corsService);
+	}
+
+	@Override
+	public synchronized Restlet createInboundRoot() {
+		final Router router = new Router(getContext());
+		DeclaredServerResources	.getDeclaredServerResources()
+								.forEach((url, resource) -> router.attach(url, resource));
+		return router;
+	}
 }
